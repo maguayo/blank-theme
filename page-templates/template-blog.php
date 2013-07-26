48a6ff5ceed785976517bdbf210d8589d9f0833f
Marcos Aguayo <marcos@yage.(none)>
2013-07-26 18:13:30 +0200

    Second version

---
+++ page-templates/template-blog.php
@@ -0,0 +1,4 @@
+<?php /* Template Name: Blog */ ?>
+<?php get_header(); ?>
+
+<?php get_footer(); ?>
---
+++ page-templates/template-home.php
@@ -0,0 +1,4 @@
+<?php /* Template Name: Home */ ?>
+<?php get_header(); ?>
+
+<?php get_footer(); ?>
