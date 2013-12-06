e2ac3da363e8387ea76013c23d184c057765384d
Marcos Aguayo <marcosaguayomora@gmail.com>
2013-12-06 15:03:59 +0100

    Añadiendo funciones para twitter

--- functions.php
+++ functions.php
@@ -141,4 +141,103 @@ function custom_admin_js() {
 }
 add_action('admin_footer', 'custom_admin_js');
 
+
+function custom_excerpt($limit) {
+    $excerpt = explode(' ', get_the_excerpt(), $limit);
+    if(count($excerpt)>=$limit){
+        array_pop($excerpt);
+        $excerpt = implode(" ",$excerpt).'...';
+    }else{
+        $excerpt = implode(" ",$excerpt);
+    }
+    $excerpt = preg_replace('`[[^]]*]`','',$excerpt);
+    return $excerpt;
+}
+
+/* ================================================= */
+/* ==================== TWITTER ==================== */
+/* ================================================= */
+
+function buildBaseString($baseURI, $method, $params) {
+    $r = array();
+    ksort($params);
+    foreach($params as $key=>$value){
+        $r[] = "$key=" . rawurlencode($value);
+    }
+    return $method."&" . rawurlencode($baseURI) . '&' . rawurlencode(implode('&', $r));
+}
+
+function buildAuthorizationHeader($oauth) {
+    $r = 'Authorization: OAuth ';
+    $values = array();
+    foreach($oauth as $key=>$value)
+        $values[] = "$key=\"" . rawurlencode($value) . "\"";
+    $r .= implode(', ', $values);
+    return $r;
+}
+
+function returnTweet(){
+    $oauth_access_token         = "X";
+    $oauth_access_token_secret  = "X";
+    $consumer_key               = "X";
+    $consumer_secret            = "X";
+    $twitter_timeline           = "user_timeline";  //  mentions_timeline / user_timeline / home_timeline / retweets_of_me
+
+    $request = array(
+        'screen_name' => 'USERNAME',
+        'count'       => '3'
+    );
+
+    $oauth = array(
+        'oauth_consumer_key'     => $consumer_key,
+        'oauth_nonce'            => time(),
+        'oauth_signature_method' => 'HMAC-SHA1',
+        'oauth_token'            => $oauth_access_token,
+        'oauth_timestamp'        => time(),
+        'oauth_version'          => '1.0'
+    );
+
+    $oauth = array_merge($oauth, $request);
+
+    $base_info = buildBaseString("https://api.twitter.com/1.1/statuses/$twitter_timeline.json", 'GET', $oauth);
+    $composite_key = rawurlencode($consumer_secret) . '&' . rawurlencode($oauth_access_token_secret);
+    $oauth_signature = base64_encode(hash_hmac('sha1', $base_info, $composite_key, true));
+    $oauth['oauth_signature'] = $oauth_signature;
+
+    $header = array(buildAuthorizationHeader($oauth), 'Expect:');
+    $options = array( CURLOPT_HTTPHEADER => $header,
+                      CURLOPT_HEADER => false,
+                      CURLOPT_URL => "https://api.twitter.com/1.1/statuses/$twitter_timeline.json?". http_build_query($request),
+                      CURLOPT_RETURNTRANSFER => true,
+                      CURLOPT_SSL_VERIFYPEER => false);
+
+    $feed = curl_init();
+    curl_setopt_array($feed, $options);
+    $json = curl_exec($feed);
+    curl_close($feed);
+
+    return json_decode($json, true);
+}
+
+function poner_links($text){
+    $text = twitter_it($text);
+    $text = link_it($text);
+    return $text;
+}
+
+function link_it($text){
+    $text = preg_replace("/(^|[\n ])([\w]*?)((ht|f)tp(s)?:\/\/[\w]+[^ \,\"\n\r\t>]*)/is", "$1$2<a href=\"$3\">$3</a>", $text);
+    $text = preg_replace("/(^|[\n ])([\w]*?)((www|ftp)\.[^ \,\"\t\n\r>]*)/is", "$1$2<a href=\"http://$3\" >$3</a>", $text);
+    $text = preg_replace("/(^|[\n ])([a-z0-9&\-_\.]+?)@([\w\-]+\.([\w\-\.]+)+)/i", "<a href=\"mailto:$2@$3\">$2@$3</a>", $text);
+    return($text);
+}
+
+function twitter_it($text){
+    $text = preg_replace("/@(\w+)/", '<a href="http://www.twitter.com/$1" target="_blank">@$1</a>', $text); 
+    $text = preg_replace("/\#(\w+)/", '<a href="http://search.twitter.com/search?q=$1" target="_blank">#$1</a>',$text); 
+    return $text;
+}
+
+
+
 ?>
