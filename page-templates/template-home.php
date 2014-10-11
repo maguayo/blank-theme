c49184a48cc5d71c2c2e016c6aa23f8513816e6b
Marcos Aguayo <marcosaguayomora@gmail.com>
2014-10-11 15:01:14 +0200

    Update template-home.php

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -1,10 +1,10 @@
 <?php /* Template Name: Home */ ?>
-<?php get_header(); ?>
+<?php get_header();
 
 $args = array(
 	'post_type' => 'inicio'
 );
-<?php $the_query = new WP_Query( $args ); ?>
+$the_query = new WP_Query( $args ); ?>
 <?php if($the_query->have_posts()): while($the_query->have_posts()): $the_query->the_post(); ?>
     <h2><?php the_title(); ?></h2>
 <?php endwhile; endif; wp_reset_postdata();?>
